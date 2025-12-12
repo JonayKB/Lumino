3910b6b207a78c3eb58b4aad8a81f2955b5315b8
carliaal <carlagarciapachecotf@gmail.com>
2025-12-12 16:33:13 +0000

    feat: signal added

---
+++ lumino/accounts/signals.py
@@ -0,0 +1,13 @@
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from users.models import Profile
+from django.conf import settings
+from django.contrib.auth import get_user_model
+
+
+@receiver(post_save, sender=get_user_model())
+def create_profile_signal(sender, instance, created, raw, using, update_fields, **kwargs):
+    if created and not raw:
+        Profile.objects.create(user=instance)
+
+
