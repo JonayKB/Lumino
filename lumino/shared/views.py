ec76244758369fa02d3a4432a1523c1eca4bde8b
JonayKB <jonaykb@gmail.com>
2025-12-12 15:30:09 +0000

    Added i18n

--- lumino/shared/views.py
+++ lumino/shared/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 
 def index(request):
-    if request.user.is_authenticated():
+    if request.user.is_authenticated:
         return redirect('subjects:subject-list')
-    return render(request, 'index')
+    return render(request, 'index.html')
---
+++ lumino/subjects/tasks.py
@@ -0,0 +1,2 @@
+def deliver_certificate():
+    pass
