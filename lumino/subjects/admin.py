6bb1c411bf24ed59cd063935325f914524c10a18
carliaal <carlagarciapachecotf@gmail.com>
2025-12-12 17:18:47 +0000

    feat: subject and enrollment detail in line

--- lumino/subjects/admin.py
+++ lumino/subjects/admin.py
@@ -1,9 +1,13 @@
 from django.contrib import admin
 from .models import Subject, Lesson, Enrollment
 
+class SubjectEnrollmentDetailInLine(admin.TabularInline):
+    model = Enrollment
+    extra = 1
+
 @admin.register(Subject)
 class SubjectAdmin(admin.ModelAdmin):
-    pass
+    inlines = [SubjectEnrollmentDetailInLine]
 
 @admin.register(Lesson)
 class LessonAdmin(admin.ModelAdmin):
