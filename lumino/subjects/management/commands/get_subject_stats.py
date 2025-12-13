6c527e7dc69f1cfb58124cef0c9a41c4eb9b4ed1
carliaal <carlagarciapachecotf@gmail.com>
2025-12-13 16:44:14 +0000

    feat: command added

---
+++ lumino/subjects/management/commands/get_subject_stats.py
@@ -0,0 +1,22 @@
+from django.core.management.base import BaseCommand, CommandError
+from subjects.models import Enrollment, Subject
+
+
+
+class Command(BaseCommand):
+    help = 'Get average score from each subject'
+
+    def handle(self, *args, **options):
+        subjects = Subject.objects.all()
+        for subject in subjects:
+            marks = subject.enrollments.filter(mark__isnull=False)
+            marks_len = marks.count()
+
+            if marks_len > 0:
+                marks_sum = sum(mark.mark for mark in marks)
+                marks_avg = marks_sum / marks_len
+            else:
+                marks_avg = 0.00
+
+            formatted_average = f'{marks_avg:.2f}'
+            self.stdout.write(f'{subject.code}: {formatted_average}')
