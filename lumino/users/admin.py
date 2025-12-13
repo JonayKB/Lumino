fc31a2f676efbf388f3776bed267574a55660dbd
popadron <dodaojya.dev@gmail.com>
2025-12-13 18:03:33 +0000

    feat: Admin Profile view now has specified fields

--- lumino/users/admin.py
+++ lumino/users/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
+
 from .models import Profile
+
 
 @admin.register(Profile)
 class ProfileAdmin(admin.ModelAdmin):
-    pass
+    list_display = ('id', 'user', 'role', 'avatar', 'user_id', 'bio')
